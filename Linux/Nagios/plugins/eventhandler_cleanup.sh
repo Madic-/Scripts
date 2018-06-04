98997d9c7734136df7949544e44f3d705306da16
Madic- <3735459+Madic-@users.noreply.github.com>
2018-06-05 01:41:03 +0200

    Renamed eventhandler_ssh to eventhandler_cleanup and refactored code

---
+++ Linux/Nagios/plugins/eventhandler_cleanup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+### Use with nagios event handler. 
+### Based on https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/eventhandlers.html
+### Written for check_mk and https://www.geekbundle.org/selbstheilung-mit-check_mk-event-handler
+###
+### Written by Madic - madic@geekbundle.org 2018-06
+
+SERVICESTATE="$1"
+SERVICESTATETYPE="$2"
+HOSTADDRESS="$3"
+LOG=~/var/log/eventhandler.log
+
+exec &>"$LOG"
+
+case "$SERVICESTATE" in
+OK)
+	;;
+WARNING)
+	;;
+UNKNOWN)
+	;;
+CRITICAL)
+	case "$SERVICESTATETYPE" in
+	SOFT)
+        ;;
+	HARD)
+        echo "$(date +"%Y-%m-%d %H:%M:%S") Doing cleanup on $HOSTADDRESS through ${0##*/} Script..."
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@"$HOSTADDRESS" sudo /usr/local/sbin/cleanup.sh
+		;;
+	esac
+	;;
+esac
+exit 0
